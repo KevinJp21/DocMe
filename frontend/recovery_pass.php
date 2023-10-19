3dcd2bf45ec426af6899f23ec26e6901c93bf4dc
KevinJp21 <kevinjp821@gmail.com>
2023-10-18 22:25:54 -0500

    .

--- frontend/recovery_pass.php
+++ frontend/recovery_pass.php
@@ -13,7 +13,7 @@ include_once '../backend/recovery_pass.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Recuperar contraseña|DocMe</title>
+    <title>Recuperar contraseña</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../frontend/css/login.css">
@@ -21,7 +21,7 @@ include_once '../backend/recovery_pass.php'
     <link rel="icon" href="../img/logo_docme.svg">
 <body>
     <div class="contenedor">
-        <img class="img-fluid logo-img" src="../img/logo-login.svg" alt="Logo DocMe">
+        <img class="img-fluid logo-img" src="../img/logo_docme.svg" alt="Logo DocMe">
         <div class="contenido-login">
             <form autocomplete="off" method="POST"  role="form">
             <?php
